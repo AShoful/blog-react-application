8b0c876f64d034d7a5031ba90fb537cd22e7a117
Angrey Shoful <shoful.a@gmail.com>
2021-04-05 23:11:31 +0300

    Updates

---
+++ precache-manifest.68f5e1240d8a801adb2ee787d11730d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdb1e1af40254f60378c46b6cac1fcc",
+    "url": "/blog-react-application/index.html"
+  },
+  {
+    "revision": "26c2bd0529f2f574bfe8",
+    "url": "/blog-react-application/static/css/main.c5d9106d.chunk.css"
+  },
+  {
+    "revision": "5c345acb3afc07601c1a",
+    "url": "/blog-react-application/static/js/2.da30c281.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/blog-react-application/static/js/2.da30c281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c2bd0529f2f574bfe8",
+    "url": "/blog-react-application/static/js/main.af8099eb.chunk.js"
+  },
+  {
+    "revision": "3dd8c493bd4a7b5fd045",
+    "url": "/blog-react-application/static/js/runtime-main.92aedbd0.js"
+  }
+]);
